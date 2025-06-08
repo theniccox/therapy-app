557f36bf9364b51bcc68613acd35e9cb903190e3
theniccox <n.alexandercox@gmail.com>
2025-06-08 16:52:40 +0100

    Update tailwind config with custom animations and colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
     extend: {
       colors: {
         brand: {
-          light: '#e6f0e4',
+          light: '#007200',
           DEFAULT: '#7c9a77',
           dark: '#4e644d',
         },
